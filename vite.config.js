cd51d6e4a5fcdb20c11047675f6033ad6ed9f5f0
ZCY <1036508235@qq.com>
2024-11-20 16:58:40 +0800

    project init

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/sunrise/',
+  build: {
+    outDir: 'docs'
+  },
   plugins: [vue()],
   resolve: {
     alias: {
